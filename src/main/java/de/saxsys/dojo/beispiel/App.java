2d4dfee06c66c82388a1fef25e765afbe8e18c10
Stefan Bley <stefan.bley@saxsys.de>
2011-04-13 14:58:29 +0200

    initial commit

---
+++ src/main/java/de/saxsys/dojo/beispiel/App.java
@@ -0,0 +1,12 @@
+package de.saxsys.dojo.beispiel;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+		"Hallo".isEmpty();
+		System.out.println("Hello World!");
+	}
+}
